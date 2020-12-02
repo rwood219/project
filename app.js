a337a240e45ce8f060c2aa467d5c474313a0785e
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-01 20:36:46 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('weather/:latlong', async (req, res) => {
     const api_key = process.env.API_KEY
     const lat = req.params.lat
     const long = req.params.long
-    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}`
+    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
     const fetch_response = await fetch(api_url)
     const json = await fetch_response.json()
     res.json(json)
