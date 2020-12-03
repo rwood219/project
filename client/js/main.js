9e689b0f2d4e6577be096adbc22e1df2d9f4305a
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-02 19:57:17 -0500

    Add files via upload

--- client/js/main.js
+++ client/js/main.js
@@ -1,63 +1,35 @@
-let long;
-let lat;
-
 if (!navigator.geolocation) {
-  alert("geolocate required");
-} else
-  navigator.geolocation.getCurrentPosition((position) => {
-    long = position.coords.longitude;
+  alert("app requires geolocation to be enabeled");
+} else {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    lon = position.coords.longitude;
     lat = position.coords.latitude;
-    console.log(position.coords.latitude, position.coords.longitude);
+    const api_url = `weather/${lat},${lon}`;
+    const response = await fetch(api_url);
+    const json = await response.json();
+    data = json;
 
-    //api key
-    const apiKey = "__________";
-    //current weather URL
-    let currentWeatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
-    //extended forcast URL
-    let oneCallWeatherApi = `http://api.openweathermap.org/data/2.5/onecall?lat=39.3425259&lon=-74.4710183999&units=imperial&appid=${apiKey}`;
-
-    //shorten some code for selecting elements from dom
     getElements = (attr) => {
       return document.querySelector(attr);
     };
+    getAllElements = (attr) => {
+      return document.querySelectorAll(attr);
+    };
 
-    //Extended forcast data
-    fetch(oneCallWeatherApi)
-      .then((response) => response.json())
-      .then((data) => {
-        data = data;
-        console.log(data);
-        const { alerts, current, daily, hourly } = data;
-        getElements("#alert-text").textContent = alerts[0].description;
-      });
+    const { current, hourly, daily, alerts } = data;
+    console.log(data);
 
-    //fetch geoLocation url
-    fetch(currentWeatherApi)
-      .then((response) => response.json())
-      .then((data) => {
-        data = data;
-        const { name, coord, weather, wind, main } = data;
-        console.log(data);
-        //set some ui stuff for current locations current weather
-        getElements("#current-temp").textContent = main.temp + " F";
-        getElements("#current-wind").textContent = wind.speed + " MPH";
-        getElements("#description").textContent = weather[0].description;
-        getElements("h1").textContent = `Current Weather For ${name}`;
-      });
-
-    //check for geolocation and get position return; JSON from api
-    //need to hook up to app.js; this will replace some temp code above like fetch and api key etc..
-    if (!navigator.geolocation) {
-      alert("app requires geolocation to be enabeled");
-    } else {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        lon = position.coords.longitude;
-        lat = position.coords.latitude;
-        console.log(lon, lat);
-        const api_url = `weather/${lat},${long}`;
-        const response = await fetch(api_url);
-        const json = await response.json();
-        console.log(json);
-      });
+    //set some ui stuff for current locations current weather
+    getElements("#current-temp").textContent = current.temp + " F";
+    getElements("#current-wind").textContent = current.wind_speed + " MPH";
+    getElements("#description").textContent = current.weather[0].description;
+    getElements("h1").textContent = `Current Weather For ${name} ${lon}${lat}`;
+    //loop api for extended forcast temp
+    console.log(current.temp);
+    for (let i = 0; i < daily.length; i++) {
+      console.log(`daily temp ${daily[i].temp.day}`);
+      console.log(`Hour temp ${hourly[i].temp}`);
     }
+    getElements("#alert-text").textContent = alerts[0].description;
   });
+}
