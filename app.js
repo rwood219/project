dfb1f4a73de18d49ec354aa8a999df434a9cb801
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-02 19:05:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ require('dotenv').config();
 app.use(express.static( __dirname + '/public'));
 app.use(express.static(__dirname + '/client'))
 
-app.get('weather/:latlong', async (req, res) => {
+app.get('/weather/:latlong', async (req, res) => {
     const api_key = process.env.API_KEY
-    const lat = req.params.lat
-    const long = req.params.long
-    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
+    const latlong = req.params.latlong.split(',');
+    console.log(latlong)
+    const lat = latlong[0];
+    const long = latlong[1]
+
+
+    const apiKey = "7d60d961f40b12ba72fbcb6211117dd0";
+    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}`
     const fetch_response = await fetch(api_url)
     const json = await fetch_response.json()
     res.json(json)
+    console.log(json)
 })
 
 const PORT = process.env.PORT || 3000;
