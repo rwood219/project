787e0952fb24466b9a7c23c8fb90a47512a389b7
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-02 19:57:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require('express')
-const fetch = require('node-fetch')
-const { response } = require('express');
-const { request } = require('http');
+const express = require("express");
+const fetch = require("node-fetch");
+const { response } = require("express");
+const { request } = require("http");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-app.use(express.static( __dirname + '/public'));
-app.use(express.static(__dirname + '/client'))
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/client"));
 
-app.get('/weather/:latlong', async (req, res) => {
-    const api_key = process.env.API_KEY
-    const latlong = req.params.latlong.split(',');
-    console.log(latlong)
-    const lat = latlong[0];
-    const long = latlong[1]
-    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}`
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    res.json(json)
-    console.log(json)
-})
+app.get("/weather/:latlong", async (req, res) => {
+  const api_key = process.env.API_KEY;
+  const latlong = req.params.latlong.split(",");
+  const lat = latlong[0];
+  const long = latlong[1];
+  const api_url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${api_key}`;
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  res.json(json);
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , () => console.log(`server running on ${PORT}`))
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
