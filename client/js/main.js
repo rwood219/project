f252bcf77a5680cbeb233199fccba6c2e7fea404
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-04 00:18:29 -0500

    Add files via upload

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,10 @@
+getElements = (attr) => {
+  return document.querySelector(attr);
+};
+getAllElements = (attr) => {
+  return document.querySelectorAll(attr);
+};
+
 if (!navigator.geolocation) {
   alert("app requires geolocation to be enabeled");
 } else {
@@ -9,27 +16,35 @@ if (!navigator.geolocation) {
     const json = await response.json();
     data = json;
 
-    getElements = (attr) => {
-      return document.querySelector(attr);
-    };
-    getAllElements = (attr) => {
-      return document.querySelectorAll(attr);
-    };
-
-    const { current, hourly, daily, alerts } = data;
+    const { current, hourly, daily, alerts, timezone } = data;
+    console.log(hourly);
     console.log(data);
 
-    //set some ui stuff for current locations current weather
-    getElements("#current-temp").textContent = current.temp + " F";
-    getElements("#current-wind").textContent = current.wind_speed + " MPH";
-    getElements("#description").textContent = current.weather[0].description;
-    getElements("h1").textContent = `Current Weather For ${name} ${lon}${lat}`;
-    //loop api for extended forcast temp
-    console.log(current.temp);
-    for (let i = 0; i < daily.length; i++) {
-      console.log(`daily temp ${daily[i].temp.day}`);
-      console.log(`Hour temp ${hourly[i].temp}`);
+    getElements("h1").textContent = `Current Weather For ${timezone}`;
+
+    //function to set hour forcast info
+    hourForcastData = (temp, wind, description) => {
+      for (let i = 0; i < hourly.length; i++) {
+        temp = hourly[i].temp;
+        wind = hourly[i].wind_speed;
+        description = hourly[i].weather[0].description;
+        console.log(temp);
+        console.log(wind);
+        console.log(description);
+      }
+    };
+    hourForcastData();
+
+    //loop for hour forcast; most of this will be scraped
+    for (let i = 0; i < 10; i++) {
+      const { feels_like, temp, weather } = data.hourly[i];
+      let hourDiv = document.createElement("div");
+      hourDiv.textContent = `${temp}${"  "}${feels_like}`;
+      getElements("#day-one").appendChild(hourDiv);
+      console.log(weather[0].description);
+      console.log(feels_like);
     }
+
     getElements("#alert-text").textContent = alerts[0].description;
   });
 }
