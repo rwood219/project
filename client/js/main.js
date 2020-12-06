ee2940e0f42f79514d15969e8da1140595f80be9
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-12-05 22:05:17 -0500

    Add files via upload

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,8 @@
-getElements = (attr) => {
-  return document.querySelector(attr);
-};
-getAllElements = (attr) => {
-  return document.querySelectorAll(attr);
-};
+getElements = (attr) => document.querySelector(attr);
 
+getAllElements = (attr) => document.querySelectorAll(attr);
+
+//check for geolocation ande get json
 if (!navigator.geolocation) {
   alert("app requires geolocation to be enabeled");
 } else {
@@ -15,36 +13,29 @@ if (!navigator.geolocation) {
     const response = await fetch(api_url);
     const json = await response.json();
     data = json;
-
     const { current, hourly, daily, alerts, timezone } = data;
-    console.log(hourly);
-    console.log(data);
+    //set weather alert sidebar
+    getElements("#alert-text").textContent = alerts[0].description;
 
+    console.log(data);
+    //set header
     getElements("h1").textContent = `Current Weather For ${timezone}`;
 
-    //function to set hour forcast info
-    hourForcastData = (temp, wind, description) => {
-      for (let i = 0; i < hourly.length; i++) {
-        temp = hourly[i].temp;
-        wind = hourly[i].wind_speed;
-        description = hourly[i].weather[0].description;
-        console.log(temp);
-        console.log(wind);
-        console.log(description);
-      }
+    //constructor for hourly forcast data
+    function CreateNewHourForcastDiv(temp, wind, feelsLike) {
+      this.temp = temp
+      this.wind = wind
+      this.feelsLike = feelsLike
+      console.log(temp, wind, feelsLike)
     };
-    hourForcastData();
 
-    //loop for hour forcast; most of this will be scraped
-    for (let i = 0; i < 10; i++) {
-      const { feels_like, temp, weather } = data.hourly[i];
-      let hourDiv = document.createElement("div");
-      hourDiv.textContent = `${temp}${"  "}${feels_like}`;
-      getElements("#day-one").appendChild(hourDiv);
-      console.log(weather[0].description);
-      console.log(feels_like);
+    for (let i = 0; i < hourly.length; i++) {
+      hourlyData = hourly[i];
+      new CreateNewHourForcastDiv(
+        hourlyData.temp,
+        hourlyData.wind_speed,
+        hourlyData.feels_like
+      )
     }
-
-    getElements("#alert-text").textContent = alerts[0].description;
-  });
-}
+  })
+};
